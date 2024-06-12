4e29810c1f1550305774e607d7b957ec87288bff
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-06-12 23:50:05 +0100

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Umair Salam on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
